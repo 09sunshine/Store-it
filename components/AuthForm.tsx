4f22036f0ebec4320f751e5ab91b556508f5bad4
09sunshine <sanshey230456@aitpune.edu.in>
2025-07-18 01:10:29 +0530

    auth forms

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 import { z } from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {useForm} from "react-hook-form";
@@ -7,7 +10,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -24,7 +26,10 @@ const formSchema = z.object({
 type FormType = "sign-in" | "sign-up";
 
 const AuthForm = ({type} : {type: FormType}) => {
-  // 1. Define your form.
+
+  const [isLoading, setisLoading] = useState(false);
+  const [errorMessage, seterrorMessage] = useState("");
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,34 +38,69 @@ const AuthForm = ({type} : {type: FormType}) => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values)
-  }
+  };
    return (
+    <>
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex max-h-[800px] w-full max-w-[580px] flex-col justify-center space-y-6 transition-all lg:h-full lg:space-y-8">
+        <h1 className="h1 text-center text-light-100 md:text-left">{type === "sign-in" ? "Sign In" : "Sign Up"}</h1>
+        {type === 'sign-up' && 
         <FormField
           control={form.control}
-          name="username"
+          name="fullName"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
+              <div className="flex h-[78px] flex-col justify-center rounded-xl border border-light-300 px-4 shadow-drop-1">
+                <FormLabel className="body-2 w-full pt-2 text-light-100">Full Name</FormLabel>
+
+                <FormControl>
+                <Input placeholder="Enter your full name" className="shad-no-focus body-2 border-none p-0 shadow-none placeholder:text-light-200" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
+              </div>
+              <FormMessage className="body-2 ml-4 text-red" />
             </FormItem>
           )}
+          
+        />}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex h-[78px] flex-col justify-center rounded-xl border border-light-300 px-4 shadow-drop-1">
+                <FormLabel className="body-2 w-full pt-2 text-light-100">Email</FormLabel>
+
+                <FormControl>
+                <Input placeholder="Enter your email" className="shad-no-focus body-2 border-none p-0 shadow-none placeholder:text-light-200" {...field} />
+              </FormControl>
+              </div>
+              <FormMessage className="body-2 ml-4 text-red" />
+            </FormItem>
+          )}
+          
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="primary-btn h-[66px]" disabled={isLoading}>
+          {type === "sign-in" ? "Sign In" : "Sign Up"}
+
+          {isLoading && (
+            <Image src= "/assets/icons/loader.svg" alt="loader" width={24} height={24} className="ml-2 animate-spin"/>
+          )}
+        </Button>
+        {errorMessage && (
+          <p className="body-2 mx-auto w-fit rounded-xl bg-error/5 px-8 py-4 text-center text-error">*{errorMessage}</p>
+        )}
+
+        <div className="body-2 flex justify-center">
+          <p className="text-light-100">{type === "sign-in" ? "Don't have an account?" : "Already have an account?"}</p>
+          <Link href={type === "sign-in" ? "/sign-up" : "/sign-in"} className="ml-1 font-medium text-brand">{type === "sign-in" ? "Sign Up" : "Sign In"}</Link>
+        </div>
       </form>
     </Form>
-  )
-}
+    </>
+    
+  );
+};
 
 export default AuthForm;
